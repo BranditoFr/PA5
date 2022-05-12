b0abdc39b2d3eff93b20fdcf03e739056cf48d8c
Hugo <hugo.dem2@hotmail.fr>
2022-05-12 18:24:33 +0200

    separate header/footer + reorganisation index + navbar + visualisation page link data studio

---
+++ smart estate/index.php
@@ -0,0 +1,77 @@
+<?php
+include("header.php");
+?>
+
+<!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">La plateforme d'investissement immobilier intelligente</h1>
+                        <hr class="divider" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 mb-5">Nos experts en immobilier,  spécialistes de la donnée et de l'intelligence artificielle vous accompagnent dans votre investissement immobilier, pour vous assurer le meilleur choix ! </p>
+                        <a class="btn btn-primary btn-xl" href="#about">Let's go</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Comment ça marche ?</h2>
+                <p class="text-muted mb-0">Grace à nos analyses statistiques et à l'intelligence artificielle nous mettons en avant les régions, départements et villes avec le meilleur taux de rendement, nous nous basons sur différentes données comme
+                     l'évolution du prix des biens, l'accroissement de la population, les infrastructures ou encore la présence (actuelle ou future) de grandes entreprises dans ces lieux.</p>
+                <hr class="divider" />
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-gem fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Analyse des tendances</h3>
+                            <p class="text-muted mb-0">Our themes are updated regularly to keep them bug free!</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Votre tableau de bord</h3>
+                            <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-heart fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Accompagnement</h3>
+                            <p class="text-muted mb-0">Ayez toutes les cartes en main en prenant contact avec nos conseillers en gestion de patrimoine.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Ready to Publish</h3>
+                            <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+        <!-- About-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">We've got what you need!</h2>
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4">Start Bootstrap has everything you need to get your new website up and running in no time! Choose one of our open source, free to download, and easy to use themes! No strings attached!</p>
+                        <a class="btn btn-light btn-xl" href="#services">Get Started!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+
+<?php
+    include("footer.php");
+?>
